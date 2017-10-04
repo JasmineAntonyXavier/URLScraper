03fcc3938ccb2e1a3828443efca4c062378fc029
Jasmine Antony Xavier <jasjesuraj@gmail.com>
2017-10-04 08:15:45 -0400

    Cleaning up some validations

--- src/com/urlscraper/ScraperMain.java
+++ src/com/urlscraper/ScraperMain.java
@@ -3,8 +3,10 @@ package com.urlscraper;
 public class ScraperMain {
 	public static void main (String args[]) {
 
-		if (args.length != 1){
+		if (args.length < 1){
 			System.out.println("Please pass in a URL as first argument");
+		}else if (args.length > 1){
+			System.out.println("Please pass only one argument");
 		}else{
 			Scraper scrape = new Scraper();
 			scrape.scrapeUrls(args[0]);
